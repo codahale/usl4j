2b966831f264dabe214d690a5dd433bb18bb314f
Coda Hale <coda.hale@gmail.com>
2017-04-22 09:25:25 -0700

    Add package-info.

---
+++ src/main/java/com/codahale/usl4j/package-info.java
@@ -0,0 +1,21 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * The {@code com.codahale.usl4j} package provides classes for recording measurements of throughput,
+ * concurrency, and latency of running systems (e.g. databases, web applications, etc.) and building
+ * a fitted Universal Scalability Law model from them which can be used to predict bottlenecks, plan
+ * capacity, and evaluate SLAs.
+ */
+package com.codahale.usl4j;
